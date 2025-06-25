a7b78bdc19d7775c050015c9cba225aec23ed6d6
sonah5009 <sonah5009@gmail.com>
2025-06-25 14:06:50 +0900

    배열

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
 
-    // let (x,y,z) = tup;
-
-    let five_hundred = tup.0;
-    let six_point_four = tup.1;
-
-    let one = tup.2;
-
-    println!("{five_hundred}, {six_point_four}, {one}");
+    let a: [i32; 5] = [1,2,3,4,5];  // 배열이 5개의 요소
+    let a0 = a[0];
+    println!("{a0}");
+    
+    let a = [3; 5];
+    // [3, 3, 3, 3, 3]
+    let a1 = a[1];
+    println!("{a1}");
+    
+    // Runtime error
+    // 명시한 인덱스가 배열 길이보다 작은지 검사
+    // 인덱스가 배열 길이보다 크거나 같을 경우 -> 패닉 (panic)
 }
