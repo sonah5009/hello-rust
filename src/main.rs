df31082c8a93af516722aa83ee495679936cdd62
sonah5009 <sonah5009@gmail.com>
2025-06-25 17:00:18 +0900

    stack-only data: copy
    - clone 이 필요 없음
    - 컴파일 타임에 크기가 고정되는 타입은 스택에 저장되므로

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let x = 5;
+    let y = x;
 
-    println!("s1 = {}, s2 = {}", s1, s2);
+    println!("x = {}, y = {}", x, y);
+
 
 }
