07fb17d71bc46dad9b60b481812f78c267a0c019
sonah5009 <sonah5009@gmail.com>
2025-06-25 17:30:24 +0900

    Returning ownership of parameters
    - 여러 값을 반환해버림

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 fn main() {
-    // yours
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let (s2, len) = calculate_length(s1);
 
-    println!("{}", s1);
-    // println!("{}", s2);
-    // s2의 소유권이 s3으로 이동
-    println!("{}", s3);
-
-
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len()은 String의 길이를 반환합니다
 
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+    (s, length)
 }
