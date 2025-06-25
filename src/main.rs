3dcab37f5ffae8ab23c7a6ab36ab8f6561e9c63a
sonah5009 <sonah5009@gmail.com>
2025-06-25 13:20:51 +0900

    shawdoing: let 없이 재할당 불가능

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    // code convention: 
-    // 상수. 대문자, snake case
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
-    let mut x = 5; // 변수를 mutable로 만듦
-    // let x = 5;   // immutable이므로 불가능
+    let x = 1;
+    let x = x+1;
     println!("{x}");
-    x=6;
+    // let 없이 shadowing 불가능
+    // x = 3;
     println!("{x}");
 }
