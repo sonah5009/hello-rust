f4ad5562584d70d583bf2c5981325f08a1ea68e4
sonah5009 <sonah5009@gmail.com>
2025-06-25 10:32:54 +0900

    2-1 예제 주석

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::io;
+use std::io;    // prelude: 표준 라이브러리에 정의된 아이템 집합. 
+// use 문 사용: 원하는 type이 prelude에 없는 경우
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+    // 변수 기본 속성: immutable
+    // &guess -> &mut guess: 가변(mutable)로 변경
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess}"); // {} : placeholder
 }
