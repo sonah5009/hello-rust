66910573ac98689938a0a44b661b67a2c9b6cf05
sonah5009 <sonah5009@gmail.com>
2025-06-25 11:26:13 +0900

    잘못된 입력값 처리하기

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,11 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // 잘못된 입력값 처리하기
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         
         println!("You guessed: {guess}"); // {} : placeholder
         
