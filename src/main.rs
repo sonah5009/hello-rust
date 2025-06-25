211dd8b9ab0a7a16201def60531d89970ea24753
sonah5009 <sonah5009@gmail.com>
2025-06-25 17:52:24 +0900

    References and Borrowing
    - & 사용해서 그대로 넘김

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{}' is {}.", s2, len);
+    println!("The length of '{}' is {}.", s1, len);
+
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len()은 String의 길이를 반환합니다
-
-    (s, length)
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
