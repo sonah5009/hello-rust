e60e25298d9dd8e6e62c4ad0840dbf66e82c3251
sonah5009 <sonah5009@gmail.com>
2025-06-25 13:18:47 +0900

    const code convention

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    // code convention: 
+    // 상수. 대문자, snake case
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
     let mut x = 5; // 변수를 mutable로 만듦
     // let x = 5;   // immutable이므로 불가능
     println!("{x}");
