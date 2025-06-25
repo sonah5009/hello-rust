bd65578f8f2b7109bf290cef198ff961ee82cb16
sonah5009 <sonah5009@gmail.com>
2025-06-25 13:22:28 +0900

    scope 내에서의 값

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
     let x = 1;
-    let x = x+1;
-    println!("{x}");
+    {
+        let x = 2;
+        println!("scope: {x}");
+    }
+    println!("밖: {x}");
     // let 없이 shadowing 불가능
     // x = 3;
-    println!("{x}");
 }
