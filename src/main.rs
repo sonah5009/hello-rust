984c0bf67ef359a5b3de6e0a80eebfcbca6f56b7
sonah5009 <sonah5009@gmail.com>
2025-06-25 11:15:15 +0900

    let 의 재선언, trim, parse, expect 메서드, cmp

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 
 use rand::Rng;    // prelude: 표준 라이브러리에 정의된 아이템 집합. 
 // use 문 사용: 원하는 type이 prelude에 없는 경우
@@ -11,12 +11,24 @@ fn main() {
     println!("The secrete number is: {secrete_number}");
 
     println!("Please input your guess.");
-    
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // shadow: 값 덮어쓰기
+    // trim 메서드: 처음과 끝부분의 공백문자들을 제거. `5\n` -> `5`
+    // parse 메서드: 문자열 -> 숫자로 변경
+    // expect 메서드: parse 실패 시 Result Err 반환 시
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
     println!("You guessed: {guess}"); // {} : placeholder
+
+    match guess.cmp(&secrete_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
