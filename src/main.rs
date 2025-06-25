53fdddcda637740c68e4b21a3580fdf9a7131400
sonah5009 <sonah5009@gmail.com>
2025-06-25 13:27:34 +0900

    let type cast

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    let x = 1;
-    {
-        let x = 2;
-        println!("scope: {x}");
-    }
-    println!("밖: {x}");
-    // let 없이 shadowing 불가능
-    // x = 3;
+    let spaces = "   ";
+    println!("{spaces}");
+
+    let spaces = spaces.len();
+    println!("{spaces}");
+
+    // 변수의 타입 변경 불가능. let 없이. 문자열 타입, 숫자 타입이었음
+    // spaces = spaces.len();
+    // println!("{spaces}");
 }
