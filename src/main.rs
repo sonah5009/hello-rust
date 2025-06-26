773656d6fd8674dd02c2aba1f95f75d5e348a764
sonah5009 <sonah5009@gmail.com>
2025-06-26 09:27:10 +0900

    mutable reference case 2:
    mutable, immutable reference 혼용 ->서로의 scope를 겹치지 않게 함

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 fn main() {
-    // case 1: mutable reference 여러 개 -> 중괄호로 새로운 스코프 생성
+    // case 2: mutable, immutable reference 혼용 
+    // -> (immutable reference) r1, r2의 사용 종료 후, 
+    // (mutable reference) r3 생성
+    // 즉, 서로의 scope가 겹치지 않게 함
+
+    // let mut s = String::from("hello");
+
+    // let r1 = &s; // 문제없음
+    // let r2 = &s; // 문제없음
+    // let r3 = &mut s; // 큰 문제
+
+    // println!("{}, {}, and {}", r1, r2, r3);
+
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-    } // 여기서 r1이 스코프 밖으로 벗어나며, 따라서 아무 문제없이 새 참조자를 만들 수 있습니다.
+    let r1 = &s; // 문제없음
+    let r2 = &s; // 문제없음
+    println!("{} and {}", r1, r2);
+    // 이 지점 이후로 변수 r1과 r2는 사용되지 않습니다
 
-    let r2 = &mut s;
+    let r3 = &mut s; // 문제없음
+    println!("{}", r3);
 
 
 }
