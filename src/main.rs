a1926e10524af6b7717feb15352b5c7f612afa96
sonah5009 <sonah5009@gmail.com>
2025-06-26 10:11:54 +0900

    first_word 함수의 결과 저장. String의 내용물 변경

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 fn main() {
     // dangling pointer
-    let reference_to_nothing = dangle();
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();  // String을 비워서 ""으로 만듦
 }
 
-fn dangle() -> String { // String 직접 반환
-// fn dangle() -> &String { // String의 참조자를 반환하겠다고 함
-    let s = String::from("hello");
-    // this function's return type contains a borrowed value, but there is no value for it to be borrowed from
-    // 이 함수는 빌린 값을 반환하고 있으나, 빌린 실제 값이 존재하지 않는다.
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    // index, 해당 요소의 reference
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
 
-    // &s // String s의 참조자를 반환
-    // s는 dangle 함수 내에서 생성 됐으므로 함수가 끝나면 free 됨.
-    // 따라서 유효하지 않은 String을 가리키는 참조자를 반환하므로 에러가 난 것.
-
-    s // String의 직접 반환해야함
+    s.len()
 }
