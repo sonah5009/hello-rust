d17ff8e501c94d9ccdf36a267f91678fc55dedf0
sonah5009 <sonah5009@gmail.com>
2025-06-26 10:30:08 +0900

    slice: immutable  reference 이후 바로 mutable reference 불가 예시
    - s.clear()

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 fn main() {
-    let s = String::from("hello");
+    let s = String::from("hello word");
 
-    let slice = &s[0..2];
-    let slice = &s[..2];
+    let word = first_word(&s);
 
-    let len = s.len();
+    // s.clear(); // &mut String -- mutable reference 요구
+    // immutable reference 인 word가 있으므로, word 가 살아 있는 동안 mutable reference 생성 불가
+    // 컴파일 에러
 
-    let slice = &s[3..len];
-    let slice = &s[3..];
-
-    let slice = &s[0..len];
-    let slice = &s[..];
+    println!("first word: {}", word);
 
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
     // index, 해당 요소의 reference
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
