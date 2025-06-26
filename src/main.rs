f7c97699a384a5953b824c1ca21a9a6cabda1343
sonah5009 <sonah5009@gmail.com>
2025-06-26 09:08:24 +0900

    참조: 주석 설명

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    let len = calculate_length(&s1); // 소유권을 넘기지 않고 해당값을 참조할 수 있도록 함
 
     println!("The length of '{}' is {}.", s1, len);
 
@@ -10,3 +10,4 @@ fn main() {
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
